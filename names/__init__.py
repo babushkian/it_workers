0859a5976c3672e17789719abad6f976b9451d5d
babushkian <babushkian@mail.ru>
2022-04-14 10:21:48 +0600

    Теперь нормально добавил файлы из пакета с именами

---
+++ names/__init__.py
@@ -0,0 +1,4 @@
+﻿from names.firms import firm_names
+from names.first_names import first_name
+from names.second_names import second_name
+from names.last_names import last_name
---
+++ names/make_list.py
@@ -0,0 +1,11 @@
+﻿import sys
+import os.path
+
+
+copylog = open("1", "r").readlines()
+result= open("firm_names_list.txt","w", encoding="utf16")
+
+for i in copylog:
+	i = i.rstrip()
+	i = f"    '{i}',\n"	
+	result.write(i)
