3bbfbf4deb7170ea29fdb3451b1642e9301f00f6
babushkian <babushkian@mail.ru>
2022-05-19 15:05:59 +0600

    Сделал подсчет сотрудников фирмы для сверки с отчетами о рейтингах.

--- test_1.py
+++ test_1.py
@@ -20,7 +20,7 @@ basefile = 'mega_workers.db'
 if Path(basefile).exists() == False:
     basefile ='workers.db'
 
-engine = create_engine(f"sqlite:///{basefile}", echo=True)
+engine = create_engine(f"sqlite:///{basefile}", echo=False)
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
@@ -85,11 +85,75 @@ def new_rating():
         print(f'firm id: {i.firm:3d} rating: {i.firm_rating:6.2f} people in firm: {i.people_count:3d}')
 
     #print(dir(Base.metadata.tables['people']))
-    print(Base.metadata.tables['people'].columns)
+
+def странный_доступ_к_таблице():
     a = Base.metadata.tables['people']
     x = session.query(a).limit(10)
     for i in x:
         print(i.current_firm_id)
+
+
+def histiory_of_workers_in_firm(firm_id=1):
+
+    records = (session.query(PeopleFirm)
+               .filter(or_(PeopleFirm.firm_from_id == firm_id, PeopleFirm.firm_to_id == firm_id))
+               .order_by(PeopleFirm.move_to_firm_date)
+               .all()
+               )
+    dates = dict()
+    count = 0
+    for i in records:
+        if i.firm_from_id == firm_id:
+            count -= 1
+        if i.firm_to_id == firm_id:
+            count += 1
+        dates[i.move_to_firm_date] = count
+
+    for d in dates:
+        print(f'{d} - {dates[d]}')
+
+
+def worker_count_correlation_with_ratings(firm_id):
+    records = (session.query(PeopleFirm)
+               .filter(or_(PeopleFirm.firm_from_id == firm_id, PeopleFirm.firm_to_id == firm_id))
+               .order_by(PeopleFirm.move_to_firm_date)
+               .all()
+               )
+    dates = dict()
+    count = 0
+    for i in records:
+        if i.firm_from_id == firm_id:
+            count -= 1
+        if i.firm_to_id == firm_id:
+            count += 1
+        dates[i.move_to_firm_date] = count
+
+    rait_rec = session.query(FirmRating).filter(FirmRating.firm_id == firm_id).order_by(FirmRating.rate_date).all()
+    # for i in rait_rec:
+    #     print(i.rate_date, i.rating, i.workers_count)
+    matches = dict()
+
+    work_process = dates.copy()
+
+    for r in rait_rec:
+        to_del = list()
+        for d in sorted(work_process):
+            if d > r.rate_date:
+                break
+            to_del.append(d)
+
+        if len(to_del) > 0:
+            matches[r] = to_del[-1]
+        else:
+            matches[r] = None
+        for i in to_del:
+            del work_process[i]
+
+    for i in matches:
+        alter_workers_count = None if matches[i] is None else dates[matches[i]]
+        print(i.rate_date, matches[i], i.workers_count, alter_workers_count)
+
+
 
 # # проверить есть ли у кого-то на момент генерации статус UNEMPLOYED
 # проверить, что у всех людей со статусом EMPLOYED фирма, к которой они приписаны не равна None
@@ -101,5 +165,7 @@ def new_rating():
 # able_people_on_start()
 # unemp_after_sin_init()
 
-new_rating()
-
+# new_rating()
+# странный_доступ_к_таблице()
+# histiory_of_workers_in_firm(16)
+worker_count_correlation_with_ratings(7)
